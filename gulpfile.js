1ebd9c4029317ed6739ba5c4064927fe4d746b3e
MAshraf <ee.mashraf@gmail.com>
2016-07-12 15:42:35 +0300

    project init starter files

---
+++ gulpfile.js
@@ -0,0 +1,53 @@
+// grab our packages
+var gulp   = require('gulp'),
+    jshint = require('gulp-jshint'),
+    sass   = require('gulp-sass'),
+    sourcemaps = require('gulp-sourcemaps'),
+    browserSync = require('browser-sync').create();
+
+// define the default task and add the watch task to it
+gulp.task('default', ['watch', 'build-css']);
+
+// configure the jshint task
+gulp.task('jshint', function() {
+  return gulp.src('source/javascript/**/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'));
+});
+
+// configure css
+gulp.task('build-css', function() {
+  return gulp.src('source/scss/**/*.scss')
+    .pipe(sourcemaps.init())  // Process the original sources
+    .pipe(sass().on('error', sass.logError))
+    .pipe(sourcemaps.write()) // Add the map to modified source.
+    .pipe(gulp.dest('public/assets/stylesheets'))
+    .pipe(browserSync.stream());
+});
+
+// configure browser-sync
+gulp.task('browser-sync', ['build-css'], function() {
+    browserSync.init({
+      server: "./public"
+  });
+});
+
+// configure which files to watch and what tasks to use on file changes
+gulp.task('watch', ['browser-sync'], function() {
+  
+  
+    
+  gulp.watch('source/javascript/**/*.js', ['jshint']);
+  gulp.watch('source/scss/**/*.scss', ['build-css']);
+  gulp.watch("public/*.html").on('change', browserSync.reload);
+});
+
+gulp.task('build-js', function() {
+  return gulp.src('source/javascript/**/*.js')
+    .pipe(sourcemaps.init())
+      .pipe(concat('bundle.js'))
+      //only uglify if gulp is ran with '--type production'
+      .pipe(gutil.env.type === 'production' ? uglify() : gutil.noop()) 
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('public/assets/javascript'));
+});
